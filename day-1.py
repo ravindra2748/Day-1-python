9e4f7be5418a459df57bda8706da41e4ff645e56
ravindra2748 <r.shah9956@gmail.com>
2025-12-13 18:01:05 +0530

    Initial commit of ecommerce_project

---
+++ day-1.py
@@ -0,0 +1,21 @@
+
+cars = ["Ford", "Volvo", "BMW"]
+
+# '''
+# # What is an Array?
+# # An array is a special variable, which can hold more than one value at a time.
+
+# # If you have a list of items (a list of car names, for example), storing the cars in single variables could look like this:
+
+# '''
+
+car1 = "Ford"
+car2 = "Volvo"
+car3 = "BMW"
+
+x = cars[0]
+print(x)
+cars[0] = "toyota"
+print(cars[0])
+x = len(cars)
+print(x)
