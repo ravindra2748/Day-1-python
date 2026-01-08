02456aa45c303610be708d3dead872b9d76bf86a
ravindra2748 <r.shah9956@gmail.com>
2026-01-08 13:05:19 +0530

    Added day-1.py and opps.py

---
+++ day-1.py
@@ -0,0 +1,125 @@
+
+cars = ["Ford", "Volvo", "BMW"]
+
+# '''
+# # What is an Array?
+# # An array is a special variable, which can hold more than one value at a time.
+
+# # If you have a list of items (a list of car names, for example), storing the cars in single variables could look like this:
+
+# '''
+
+car1 = "Ford"
+car2 = "Volvo"
+car3 = "BMW"
+
+x = cars[0]
+print(x)
+cars[0] = "toyota"
+print(cars[0])
+x = len(cars)
+print(x)
+
+for x in cars:
+  print(x)
+
+cars.append("Honda")
+cars.pop(1)
+print(cars)
+cars.remove("Honda")
+print(cars)
+cars.copy()
+cars
+
+# Python Iterators
+
+
+# Python Iterators
+# An iterator is an object that contains a countable number of values.
+
+# An iterator is an object that can be iterated upon, meaning that you can traverse through all the values.
+
+# Technically, in Python, an iterator is an object which implements the iterator protocol, which consist of the methods __iter__() and __next__().
+
+# Iterator vs Iterable
+# Lists, tuples, dictionaries, and sets are all iterable objects. They are iterable containers which you can get an iterator from.
+
+# All these objects have a iter() method which is used to get an iterator:
+
+mytuple = ("apple", "banana", "cherry")
+myit = iter(mytuple)
+
+print(next(myit))
+print(next(myit))
+print(next(myit))
+
+mystr = "banana"
+myit = iter(mystr)
+print(next(myit))
+print(next(myit))
+print(next(myit))
+print(next(myit))
+print(next(myit))
+print(next(myit))
+# print(next(myit))
+# print(next(myit))
+# print(next(myit))
+
+mytuple = ("apple", "banana", "cherry")
+
+for x in mytuple:
+  print(x)
+
+mystr = "banana"
+
+for x in mystr:
+  print(x)
+
+class Mynumbers:
+  def __iter__(self):
+    self.a = 1
+    return self
+  
+  def __next__(self):
+    x = self.a
+    self.a +=1
+    return x
+  
+myclass = Mynumbers()
+myiter = iter(myclass)
+
+print(next(myiter))
+print(next(myiter))
+print(next(myiter))
+print(next(myiter))
+print(next(myiter))
+print(next(myiter))
+print(next(myiter))
+print(next(myiter))
+print(next(myiter))
+
+# for i in myiter:
+#   print(i)
+
+
+# class MyNumbers:
+#   def __iter__(self):
+#     self.a = 1
+#     return self
+  
+#   def __next__(self):
+#     if self.a <= 20:
+#       x = self.a
+#       self.a +=1
+#       return x
+#     else:
+#       raise StopIternation
+    
+# myclass = MyNumbers()
+# myiter = iter(myclass)
+
+# for x in myiter:
+#   print(x)
+
+
+# 
---
+++ opps.py
@@ -0,0 +1,143 @@
+class MyClass:
+    x = 5
+
+# p1 = MyClass()
+# print(p1.x)
+
+# del p1
+# print(p1.x)
+
+# Multiple object
+p1 = MyClass()
+p2 = MyClass()
+p3 = MyClass()
+
+print(p1.x)
+print(p2.x)
+print(p3.x)
+
+# class with init method
+class Person:
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+    
+p1 = Person("Email" , 36)
+
+print(p1.name)
+print(p1.age)
+
+# create class without init
+class Person:
+    pass
+
+p1 = Person()
+p1.name = "Tobias"
+p1.age = 23
+
+print(p1.name)
+print(p1.age)
+
+# default value in init
+
+class person:
+    def __init__(self,name,age=18):
+        self.name = name
+        self.age = age
+
+p1 = person("yoy")
+p2 = person("Tobia", 88)
+
+print(p1.name , p1.age)
+print(p2.name , p2.age)
+
+#multiple perameter
+## Update Log
+# - Jan 8, 2026: Added new OOP examples and datetime utilities
+
+class Person:
+    def __init__(self,name,age,city,country):
+        self.name = name
+        self.age = age
+        self.city = city
+        self.country = country
+
+p1 = Person("lion",23,"Indore","India")
+
+print(p1.name)
+print(p1.age)
+print(p1.city)
+print(p1.country)
+
+class Person:
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+
+    
+    def greet(self):
+        print("Hello"+ self.name)
+
+P1 = Person("Email", 23)
+P1.greet()
+
+class Person:
+    def __init__(self , name):
+        self.name = name
+
+    def printname(self):
+            print(self.name)
+
+p1 = Person("TOT")
+p2 = Person("ffg")
+
+p1.printname()
+p2.printname()
+
+
+class Peraon:
+    def __init__(self,name):
+        self.name = name
+
+    def printname(self):
+        print(self.name)
+
+p1 = Person("TOT")
+p2 = Person("ffg")
+
+p1.printname()
+p2.printname()
+
+# Use the words myobject and abc instead of self:
+
+class person:
+    def __init__(myobj,name):
+        myobj.name = name
+
+    def greet(abc):
+        print("Hello " + abc.name)
+
+p1 = person("uv")
+p1.greet()
+
+# Accessing Properties with self
+# You can access any property of the class using self:
+
+class Car:
+    def __init__(self,brand,model,year):
+        self.brand = brand
+        self.model = model
+        self.year = year
+
+    def display_info(self):
+        print(f"{self.year,self.model,self.brand}")
+
+    
+car1 = Car(1997,"corolla","Toyota")
+car1.display_info()
+
+
+# Python Class Properties
+
+# Class Properties
+# Properties are variables that belong to a class. They store data for each object created from the class.
