0304a3d436c8a3c20cf777bf8f330f4732144590
ravindra2748 <r.shah9956@gmail.com>
2026-01-09 11:48:10 +0530

    Update on 09/01: added new code changes

--- opps.py
+++ opps.py
@@ -141,3 +141,180 @@ car1.display_info()
 
 # Class Properties
 # Properties are variables that belong to a class. They store data for each object created from the class.
+
+class Person:
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+
+p1 = Person("Email",22)
+
+print(p1.name)
+print(p1.age)
+
+class Car:
+    def __init__(self,brand,model):
+        self.brand = brand
+        self.model = model
+
+car1 = Car("Toyota","Corolla")
+print(car1.brand)
+print(car1.model)
+
+# modify properties
+
+class Person:
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+
+p1 = Person("Tobias" , 25)
+print(p1.age)
+
+p1.age = 26
+print(p1.age)
+
+# del perameter
+
+class Person:
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+
+p1 = Person("nom", 90)
+
+del p1.age
+print(p1.name)
+# print(p1.age)
+
+# Class Properties vs Object Properties
+
+# Properties defined inside __init__() belong to each object (instance properties).
+
+# Properties defined outside methods belong to the class itself (class properties) and are shared by all objects:
+
+class Person:
+    species = "Human"
+
+    def __init__(self,name):
+        self.name = name
+
+p1 = Person("Email")
+p2 = Person("fgd")
+
+print(p1.name)
+print(p2.name)
+print(p1.species)
+print(p2.species)
+
+# Modifying Class Properties
+# When you modify a class property, it affects all objects:
+
+class Peraon:
+    lastname = ""
+
+    def __init__(self,name):
+        self.name = name
+
+p1 = Peraon("nam")
+p2 = Peraon("kam")
+
+Peraon.lastname = "fres"
+
+
+print(p1.lastname)
+print(p2.lastname)
+
+class Person:
+    def __init__(self,name):
+        self.name = name
+
+p1 = Person("monkr")
+
+p1.age = 23
+p1.city = "oslo"
+
+print(p1.name)
+print(p1.age)
+print(p1.city)
+
+# Python Class Methods
+# Class Methods
+# Methods are functions that belong to a class. They define the behavior of objects created from the class
+
+class Person:
+    def __init__(self,name):
+        self.name = name
+
+    def greet(self):
+        print("Hello, my name is " + self.name)
+
+p1 = Person("Emil")
+p1.greet()
+
+class Calculator:
+    def add(self,a,b):
+        return a+b
+    
+    def multiply(self,a,b):
+        return a*b
+    
+calc = Calculator()
+print(calc.add(5,3))
+print(calc.multiply(5,8))
+
+# Methods Accessing Properties
+# Methods can access and modify object properties using self:
+
+class Person:
+    def __init__(self,name ,age):
+        self.name = name
+        self.age = age
+
+    def get_info(self):
+        return f"{self.name} is {self.age} year old"
+    
+p1 = Person("lool", 90)
+print(p1.get_info())
+
+# Methods Modifying Properties
+# Methods can modify the properties of an object:
+
+class Person:
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+
+    def celebrate_birthday(self):
+        self.age += 1
+        print(f"Happy birthday {self.name}! you are now {self.age}")
+
+p1 = Person("lola", 89)
+p1.celebrate_birthday()
+p1.celebrate_birthday()
+
+
+# The __str__() Method
+# The __str__() method is a special method that controls what is returned when the object is printed:
+
+# Without the __str__() method:
+class Person:
+    def __init__(self , name , age):
+        self.name = name
+        self.age = age
+
+p1 = Person ("mai", 90)
+print(p1)
+
+# With the __str__() method:
+
+class person:
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+
+    def __str__(self):
+        return f"{self.name} ({self.age})"
+    
+p1 = person("lol", 90)
+print(p1)
