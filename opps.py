933bf36cf562f1eded147d79a51049aa95126397
ravindra2748 <r.shah9956@gmail.com>
2026-01-22 09:57:30 +0530

    new add 22/1/2026

--- opps.py
+++ opps.py
@@ -443,3 +443,146 @@ for x in (car1, boat1, plane1):
     print(x.brand)
     print(x.model)
     x.move()
+
+# Python Encapsulation
+'''
+Python Encapsulation
+Encapsulation is about protecting data inside a class.
+
+It means keeping data (properties) and methods together in a class, while controlling how the data can be accessed from outside the class.
+
+This prevents accidental changes to your data and hides the internal details of how your class works.
+
+Private Properties
+In Python, you can make properties private by using a double underscore __ prefix:
+'''
+
+# Private Properties
+# In Python, you can make properties private by using a double underscore __ prefix:
+# class person:
+#     def __init__(self,name,age):
+#         self.name = name
+#         self.age = age
+    
+# p1 = Person("email",23)
+# print(p1.name)
+# print(p1.__age)
+
+
+'''
+Get Private Property Value
+To access a private property, you can create a getter method:
+
+Example
+Use a getter method to access a private property:
+
+
+'''
+
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.__age = age
+
+  def get_age(self):
+    return self.__age
+
+p1 = Person("Tobias", 25)
+print(p1.get_age())
+
+# Set Private Property Value
+# To modify a private property, you can create a setter method.
+
+# The setter method can also validate the value before setting it:
+
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.__age = age
+
+  def get_age(self):
+    return self.__age
+
+  def set_age(self, age):
+    if age > 0:
+      self.__age = age
+    else:
+      print("Age must be positive")
+
+p1 = Person("Tobias", 25)
+print(p1.get_age())
+
+p1.set_age(26)
+print(p1.get_age())
+
+class Student:
+    def __init__(self,name):
+        self.name = name
+        self.__grade = 0
+
+    def set_grade(self,grade):
+        if 0 <= grade <=100:
+            self.__grade = grade
+        else:
+            print("Grade must be between 0 and 100")
+
+    def get_grade(self):
+        return self.__grade
+    
+    def get_status(self):
+        if self.__grade >= 60:
+            return "passed"
+        else:
+            return "Failed"
+        
+student = Student("Emil")
+student.set_grade(-9)
+print(student.get_grade())
+print(student.get_status())
+
+
+class Person:
+    def __init__(self,name,salary):
+        self.name = name
+        self._salary = salary
+
+p1 = Person("linus",90000)
+print(p1.name)
+print(p1._salary)
+
+class Calculator:
+  def __init__(self):
+    self.result = 0
+
+  def __validate(self, num):
+    if not isinstance(num, (int, float)):
+      return False
+    return True
+
+  def add(self, num):
+    if self.__validate(num):
+      self.result += num
+    else:
+      print("Invalid number")
+
+calc = Calculator()
+calc.add(10)
+calc.add(5)
+print(calc.result)
+# calc.__validate(5) # This would cause an error
+
+# Name Mangling
+# Name mangling is how Python implements private properties and methods.
+
+# When you use double underscores __, Python automatically renames it internally by adding _ClassName in front.
+
+# For example, __age becomes _Person__age.
+
+class Person:
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+
+p1 = Person("Emil",30)
+
+print(p1.age)
