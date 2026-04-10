b0d2b04f8d723ed35579861f9c521d699a64088f
ravindra2748 <r.shah9956@gmail.com>
2026-04-10 11:57:21 +0530

    4/10

---
+++ 03_loops/O3_solution.py
@@ -0,0 +1,13 @@
+num = 10
+# for i in range(1,11):
+#     if i !=5:
+#         print(i, "*",num ,"=", i*num)
+
+# for i in range(1,11):
+#     if i == 5:
+#         continue
+#     print(i, "*",num ,"=", i*num)
+
+
+for i in range(5) and  range(6,11):
+    print(i, "*",num ,"=", i*num)
